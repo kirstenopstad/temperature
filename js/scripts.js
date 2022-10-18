a5203625f0d6a8af85b479a11d414d1020d6e1fd
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-18 10:08:42 -0700

    Add Celsius to Farenheit functionality
    Co-authored-by: VW <vera.weikel@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,17 @@ function toCelsius() {
   window.alert("Equals " + temperatureC + " in degrees Celsius");
   return temperatureC;
 }
-  
+
+function celciusToFareneheit() {
+  let temperatureC = parseInt(window.prompt("Enter the temperature in degrees Celcius"));
+    
+  // Conversion equation F = C+32/(5/9)
+  let temperatureF = [(temperatureC + 32)/(5 / 9)];
+  window.alert("Equals " + temperatureF + " in degrees Fareneheit.");
+  return temperatureF;
+}
+
 // Output temperature in degrees Celsius
 
 toCelsius();
+celciusToFareneheit();
